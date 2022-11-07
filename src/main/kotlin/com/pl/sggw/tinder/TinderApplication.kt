e459f1ce35ddb2e9edfdaf4daa6b2e92431f810f
Kapiroszka <kapiroszka@gmail.com>
2022-11-07 19:22:52 +0100

    Init commit

---
+++ src/main/kotlin/com/pl/sggw/tinder/TinderApplication.kt
@@ -0,0 +1,11 @@
+package com.pl.sggw.tinder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TinderApplication
+
+fun main(args: Array<String>) {
+	runApplication<TinderApplication>(*args)
+}
